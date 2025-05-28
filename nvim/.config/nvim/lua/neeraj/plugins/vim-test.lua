6aafd80b999cfb71aff5fcbf9162e69c5a4e37a3
Neeraj Krishna N <neerajnjps03@gmail.com>
2025-05-28 18:39:00 +0530

    Added yazi config

--- nvim/.config/nvim/lua/neeraj/plugins/vim-test.lua
+++ nvim/.config/nvim/lua/neeraj/plugins/vim-test.lua
@@ -6,8 +6,8 @@ return {
 	vim.keymap.set("n", "<leader>t", ":TestNearest<CR>", { desc = "Test Nearest" }),
 	vim.keymap.set("n", "<leader>T", ":TestFile<CR>", { desc = "Test File" }),
 	-- vim.keymap.set("n", "<leader>a", ":TestSuite<CR>", { desc = "Test Suite" }),
-	vim.keymap.set("n", "<leader>l", ":TestLast<CR>", { desc = "Test Last" }),
-	vim.keymap.set("n", "<leader>g", ":TestVisit<CR>", { desc = "Test Visit" }),
+	-- vim.keymap.set("n", "<leader>l", ":TestLast<CR>", { desc = "Test Last" }),
+	-- vim.keymap.set("n", "<leader>g", ":TestVisit<CR>", { desc = "Test Visit" }),
 
 	vim.cmd("let test#strategy = 'vimux' "),
 }
