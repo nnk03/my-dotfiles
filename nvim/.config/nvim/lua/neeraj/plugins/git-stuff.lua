1719615c50928a301a3cba089186efbca8ccef9d
Neeraj Krishna N <neerajnjps03@gmail.com>
2025-06-20 15:26:55 +0530

    Updated Keymaps

--- nvim/.config/nvim/lua/neeraj/plugins/git-stuff.lua
+++ nvim/.config/nvim/lua/neeraj/plugins/git-stuff.lua
@@ -4,8 +4,11 @@ return {
 		event = { "BufReadPre", "BufNewFile" },
 		config = function()
 			require("gitsigns").setup()
-			vim.keymap.set("n", "<leader>gp", ":Gitsigns preview_hunk<CR>", { desc = "GitSign Preview Hunk" })
-			vim.keymap.set(
+
+			local keymap = vim.keymap
+
+			keymap.set("n", "<leader>gp", ":Gitsigns preview_hunk<CR>", { desc = "GitSign Preview Hunk" })
+			keymap.set(
 				"n",
 				"<leader>gt",
 				":Gitsigns toggle_current_line_blame<CR>",
@@ -15,5 +18,13 @@ return {
 	},
 	{
 		"tpope/vim-fugitive",
+		config = function()
+			-- require("vim-fugitive").setup()
+
+			local keymap = vim.keymap
+
+			keymap.set("n", "<leader>gs", ":Git <CR>", { desc = "Show Git Status" })
+			keymap.set("n", "<leader>gc", ":Git commit<CR>", { desc = "Show Git Status" })
+		end,
 	},
 }
