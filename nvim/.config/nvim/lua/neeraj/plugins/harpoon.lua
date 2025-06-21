f952b45ea304f2e6818d6649a9aae1e010ce164e
Neeraj Krishna N <neerajnjps03@gmail.com>
2025-06-21 11:10:17 +0530

    updated harpoon plugin

--- nvim/.config/nvim/lua/neeraj/plugins/harpoon.lua
+++ nvim/.config/nvim/lua/neeraj/plugins/harpoon.lua
@@ -19,11 +19,23 @@ return {
 			"<cmd>lua require('harpoon.ui').nav_next()<cr>",
 			{ desc = "Go to next harpoon mark" }
 		)
+
 		keymap.set(
 			"n",
 			"<leader>hp",
 			"<cmd>lua require('harpoon.ui').nav_prev()<cr>",
 			{ desc = "Go to previous harpoon mark" }
 		)
+
+		keymap.set("n", "<leader>hh", '<cmd>lua require("harpoon.ui").toggle_quick_menu()<CR>', {
+			desc = "Toggle Harpoon Menu",
+		})
+
+		keymap.set("n", "<leader>h1", '<cmd>lua require("harpoon.ui").nav_file(1)<CR>', {
+			desc = "Go to harpoon file number 1",
+		})
+		keymap.set("n", "<leader>h2", '<cmd>lua require("harpoon.ui").nav_file(2)<CR>', {
+			desc = "Go to harpoon file number 2",
+		})
 	end,
 }
