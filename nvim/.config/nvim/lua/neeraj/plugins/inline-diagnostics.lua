9f02f827204184e6e547d0b84449ed21cd6f3a8d
Neeraj Krishna N <neerajnjps03@gmail.com>
2025-07-12 11:18:29 +0530

    updated to add functionality to copy inline diagnostics

--- nvim/.config/nvim/lua/neeraj/plugins/inline-diagnostics.lua
+++ nvim/.config/nvim/lua/neeraj/plugins/inline-diagnostics.lua
@@ -5,5 +5,18 @@ return {
 	config = function()
 		require("tiny-inline-diagnostic").setup()
 		vim.diagnostic.config({ virtual_text = false }) -- Only if needed in your configuration, if you already have native LSP diagnostics
+		vim.keymap.set("n", "<leader>cd", function()
+			local diags = vim.diagnostic.get(0, { lnum = vim.fn.line(".") - 1 })
+			local messages = {}
+			for _, d in ipairs(diags) do
+				table.insert(messages, d.message)
+			end
+			local result = table.concat(messages, "\n")
+			vim.fn.setreg("+", result)
+			print("Copied diagnostics to clipboard!")
+		end, { desc = "Copy diagnostics on current line" })
+
+		-- :lua print(vim.inspect(vim.diagnostic.get(0, {lnum = vim.fn.line('.') - 1})))
+		-- :lua for _, d in ipairs(vim.diagnostic.get(0, {lnum = vim.fn.line('.') - 1})) do print(d.message) end
 	end,
 }
