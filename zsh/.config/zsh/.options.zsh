6fb507caa0978930e4bc66edd4337167324451f9
Neeraj Krishna N <neerajnjps03@gmail.com>
2025-10-05 09:05:36 +0530

    updated .zshrc

--- zsh/.config/zsh/.options.zsh
+++ zsh/.config/zsh/.options.zsh
@@ -20,17 +20,17 @@ eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
 
 
 # for screensharing
-export XDG_CURRENT_DESKTOP=sway
-export XDG_SESSION_TYPE=wayland
-export QT_QPA_PLATFORM=wayland
-export SDL_VIDEODRIVER=wayland
+# export XDG_CURRENT_DESKTOP=sway
+# export XDG_SESSION_TYPE=wayland
+# export QT_QPA_PLATFORM=wayland
+# export SDL_VIDEODRIVER=wayland
 
 bindkey '^ ' autosuggest-accept
 bindkey '^o' clear-screen
 
 
-export PATH="$HOME/.nvim-linux-x86_64/bin/./:$PATH"
-export PATH="$HOME/.cargo/bin:$PATH"
+# export PATH="$HOME/.nvim-linux-x86_64/bin/./:$PATH"
+# export PATH="$HOME/.cargo/bin:$PATH"
 # export PATH="$HOME/.alacritty/target/release:$PATH"
 # alacritty can be installed using sudo apt in ubuntu 24.04
 
@@ -40,15 +40,15 @@ export PATH="$HOME/.cargo/bin:$PATH"
 # conda deactivate
 
 # Created by `pipx` on 2024-08-07 12:04:55
-export PATH="$PATH:/home/neeraj/.local/bin"
+# export PATH="$PATH:/home/neeraj/.local/bin"
 
 
 # For CUDA
-export PATH=/usr/local/cuda-12.6/bin${PATH:+:${PATH}}
-export LD_LIBRARY_PATH=/usr/local/cuda-12.6/lib64${LD_LIBRARY_PATH:+:${LD_LIBRARY_PATH}}
+# export PATH=/usr/local/cuda-12.6/bin${PATH:+:${PATH}}
+# export LD_LIBRARY_PATH=/usr/local/cuda-12.6/lib64${LD_LIBRARY_PATH:+:${LD_LIBRARY_PATH}}
 
 # For Go
-export PATH=$PATH:/usr/local/go/bin
+# export PATH=$PATH:/usr/local/go/bin
 
 
 
@@ -58,18 +58,18 @@ source <(fzf --zsh)
 # source $HOME/.venv/bin/activate
 
 # for networkx directory alone 
-if [[ $(pwd) == "$HOME/btp/networkx"* ]]; then
-   source "$HOME/btp/networkx/networkx-dev/bin/activate"
-fi
+# if [[ $(pwd) == "$HOME/btp/networkx"* ]]; then
+#    source "$HOME/btp/networkx/networkx-dev/bin/activate"
+# fi
+#
+# if [[ $(pwd) == "$HOME/Desktop/learning/ml/kaggle-titanic"* ]]; then
+#    source "$HOME/Desktop/learning/ml/kaggle-titanic/.kaggle/bin/activate"
+# fi
 
-if [[ $(pwd) == "$HOME/Desktop/learning/ml/kaggle-titanic"* ]]; then
-   source "$HOME/Desktop/learning/ml/kaggle-titanic/.kaggle/bin/activate"
-fi
-
-export GUROBI_HOME="/opt/gurobi/linux64"
-export PATH="${GUROBI_HOME}/bin:$PATH"
-export LD_LIBRARY_PATH="${GUROBI_HOME}/lib:$LD_LIBRARY_PATH"
-export GRB_LICENSE_FILE="$HOME/gurobi.lic"
+# export GUROBI_HOME="/opt/gurobi/linux64"
+# export PATH="${GUROBI_HOME}/bin:$PATH"
+# export LD_LIBRARY_PATH="${GUROBI_HOME}/lib:$LD_LIBRARY_PATH"
+# export GRB_LICENSE_FILE="$HOME/gurobi.lic"
 
 
 # for yazi
