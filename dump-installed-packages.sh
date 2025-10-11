d93ae37c1c53c3ff2fff22cabaa93a61c97def32
Neeraj Krishna N <neerajnjps03@gmail.com>
2025-10-11 21:12:21 +0530

    removed yay and install paru

--- dump-installed-packages.sh
+++ dump-installed-packages.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
+# paru is preferred than yay
+
 pacman -Qe > pacman_with_version.txt
-yay -Qe > yay_with_version.txt
+paru -Qe > yay_with_version.txt
 pacman -Qe | awk ' { print $1 } ' > pacman_without_version.txt
-yay -Qe | awk ' { print $1 } ' > yay_without_version.txt
+paru -Qe | awk ' { print $1 } ' > yay_without_version.txt
 
