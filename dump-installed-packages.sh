df6fc162df2a4c25a65d8155f27cc9227adeac84
Neeraj Krishna N <neerajnjps03@gmail.com>
2025-10-15 08:33:42 +0530

    updated files

--- dump-installed-packages.sh
+++ dump-installed-packages.sh
@@ -3,7 +3,7 @@
 # paru is preferred than yay
 
 pacman -Qe > pacman_with_version.txt
-paru -Qe > yay_with_version.txt
+paru -Qe > paru_with_version.txt
 pacman -Qe | awk ' { print $1 } ' > pacman_without_version.txt
-paru -Qe | awk ' { print $1 } ' > yay_without_version.txt
+paru -Qe | awk ' { print $1 } ' > paru_without_version.txt
 
