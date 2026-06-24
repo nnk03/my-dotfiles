776ea7e8ee0ba764f1c7f8d2debcf68b427bd1ce
Neeraj Krishna N <neerajnjps03@gmail.com>
2026-06-24 16:28:19 +0530

    added sesh tmux session manager package using copr and added some config
    Sesh Tmux session manager : https://github.com/joshmedeski/sesh

--- zsh/.config/zsh/.options.zsh
+++ zsh/.config/zsh/.options.zsh
@@ -99,3 +99,20 @@ setopt HIST_IGNORE_SPACE
 [ -f "/home/neeraj/.ghcup/env" ] && . "/home/neeraj/.ghcup/env" # ghcup-env
 
 # source /usr/share/nvm/init-nvm.sh
+
+function sesh-sessions() {
+  {
+    exec </dev/tty
+    exec <&1
+    local session
+    session=$(sesh list -t -c | fzf --height 40% --reverse --border-label ' sesh ' --border --prompt '⚡  ')
+    zle reset-prompt > /dev/null 2>&1 || true
+    [[ -z "$session" ]] && return
+    sesh connect $session
+  }
+}
+
+zle     -N             sesh-sessions
+bindkey -M emacs '\es' sesh-sessions
+bindkey -M vicmd '\es' sesh-sessions
+bindkey -M viins '\es' sesh-sessions
