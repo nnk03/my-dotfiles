a8197126cb4a2fdacb3eb63e96da9b5933028508
Neeraj Krishna N <neerajnjps03@gmail.com>
2026-08-06 22:38:36 +0530

    updated tex related vim configurations

--- nvim/.config/nvim/lua/plugins/vimtex.lua
+++ nvim/.config/nvim/lua/plugins/vimtex.lua
@@ -2,6 +2,12 @@ return {
    "lervag/vimtex",
    lazy = false,
    init = function()
+      -- vim.g.vimtex_indent_ignored_envs = { 'document' }
+
+      -- disable opening of quickfix every time, file is compiled
+      -- we can still open the quickfix window using <localleader>le
+      vim.g.vimtex_quickfix_open_on_warning = 0
+
       -- Disable VimTeX's version check to bypass the v0.11 false positive
       vim.g.vimtex_version_check = 0
 
